64efd38f0fc90e0db28e80f471fbdd59baba179c
Connor Stein <connor.stein@mail.mcgill.ca>
2019-11-03 18:02:06 -0500

    rename

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,9 @@ extern crate log;
 
 use std::str;
 use std::sync::{Arc, RwLock};
+use std::string::String;
 use std::vec::Vec;
-use log::{info
-};
+use log::{info, warn};
 
 use futures::{future, Stream};
 use futures::future::Future;
@@ -19,42 +19,75 @@ use hyper::StatusCode;
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 
-#[derive(Serialize, Deserialize, Debug, Copy, Clone)] // Deriving traits we need
-pub struct Trade {
-    amount: i32,
+
+#[derive(Serialize, Deserialize, Debug, Copy, Clone)]
+enum Side {
+    Buy,
+    Sell
 }
+
+#[derive(Serialize, Deserialize, Debug, Copy, Clone)]
+enum Symbol {
+    IBM,
+    AAPL,
+    AMZN
+}
+
+#[derive(Serialize, Deserialize, Debug, Copy, Clone)]
+enum OrderType {
+    MKT,
+    // TODO: limit, fok, etc.
+}
+
+#[derive(Serialize, Deserialize, Debug, Copy, Clone)] // Deriving traits we need
+pub struct Order {
+    amount: u32,
+    symbol: Symbol,
+    side: Side,
+    order_type: OrderType,
+}
+
 
 type GenericError = Box<dyn std::error::Error + Send + Sync>;
 type ResponseFuture = Box<dyn Future<Item = Response<Body>, Error = GenericError> + Send>;
-type Trades = Arc<RwLock<Vec<Trade>>>;
+type OrderBook = Arc<RwLock<Vec<Order>>>;
 
 // Back this "exchange" with an in-memory store
 lazy_static! {
-    pub static ref TRADES: Trades = Arc::new(RwLock::new(Vec::new()));
+    pub static ref ORDER_BOOK: OrderBook = Arc::new(RwLock::new(Vec::new()));
 }
 
-pub fn add_trade(t: Trade) {
-    info!("adding trade {:?}", t);
-    let trades = Arc::clone(&TRADES);
-    let mut lock = trades.write().unwrap(); // take the write lock
+pub fn add_order(t: Order) {
+    info!("adding Order {:?}", t);
+    let ob = Arc::clone(&ORDER_BOOK);
+    let mut lock = ob.write().unwrap(); // take the write lock
     lock.push(t);
 }
 
-pub fn get_trades() -> Vec<Trade> {
-    let trades = Arc::clone(&TRADES);
-    let lock = trades.read().unwrap(); // take a read lock
+pub fn get_order_book() -> Vec<Order> {
+    let ob = Arc::clone(&ORDER_BOOK);
+    let lock = ob.read().unwrap(); // take a read lock
     (*lock).clone()
 }
 
+
+// Walk through all the available OrderBook and see if we
+//pub fn fill_market_order(t: Order) -> Result<> {
+//
+//}
+
 #[cfg(test)]
 mod tests {
-    use crate::{add_trade, Trade, get_trades, Trades};
+    use crate::{Order, add_order, get_order_book};
+    use crate::Side::Buy;
+    use crate::Symbol::AAPL;
+    use crate::OrderType::MKT;
 
     #[test]
-    fn test_add_trade() {
-        add_trade(Trade{amount: 10});
-        let trades = get_trades();
-        assert_eq!(1, trades.len());
+    fn test_Order() {
+        add_order(Order{amount: 10, symbol: AAPL, side: Buy, order_type: MKT});
+        let ob = get_order_book();
+        assert_eq!(1, ob.len());
     }
 }
 
@@ -67,10 +100,10 @@ pub fn router(req: Request<Body>, _client: &Client<HttpConnector>) -> ResponseFu
                     .from_err()
                     .and_then(|whole_body| {
                         let str_body = String::from_utf8(whole_body.to_vec()).unwrap();
-                        let res: Result<Trade> = serde_json::from_str(&str_body);
+                        let res: Result<Order> = serde_json::from_str(&str_body);
                         match res {
-                            Ok(t) => {
-                                add_trade(t);
+                            Ok(v) => {
+                                add_order(v);
                                 Box::new(future::ok(
                                     Response::builder()
                                         .status(StatusCode::OK)
@@ -78,7 +111,8 @@ pub fn router(req: Request<Body>, _client: &Client<HttpConnector>) -> ResponseFu
                                         .unwrap(),
                                 ))
                             },
-                            Err(_) => {
+                            Err(e) => {
+                                warn!("bad request {}", e);
                                 Box::new(future::ok(
                                     Response::builder()
                                         .status(StatusCode::BAD_REQUEST)
@@ -92,9 +126,7 @@ pub fn router(req: Request<Body>, _client: &Client<HttpConnector>) -> ResponseFu
             )
         },
         (&Method::GET, "/") => {
-            //let trades = Arc::clone(&TRADES);
-           // let lock = trades.read().unwrap();
-            let payload = serde_json::to_string(&get_trades()).unwrap();
+            let payload = serde_json::to_string(&get_order_book()).unwrap();
             Box::new(future::ok(
                 Response::builder()
                     .status(StatusCode::OK)
